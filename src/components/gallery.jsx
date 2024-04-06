122da5b37720d27ebe5c2209628c032f4b0fe4b5
andrewdupere <andrewdupere@outlook.com>
2024-04-06 00:00:00 -0400

    update for 2024-04-06 00:00:00

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,7 +1,103 @@
-import { Image } from "./image";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Plot from "react-plotly.js";
+import axios from "axios";
+import { OrderOfSplitName } from "i18n-iso-countries";
 
 export const Gallery = (props) => {
+  const [showMap, setShowMap] = useState(false);
+  const [mapData, setMapData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://18.118.218.230:8080/metrics");
+        const data = response.data;
+
+        const cityData = data
+          .filter((item) => item.city)
+          .map((item) => ({
+            city: item.city,
+            ip: item.ip,
+            time: item.time,
+          }));
+
+        const geocoder = new window.geolite2.init({
+          // Provide your GeoLite2 license key here
+          licenseKey: "YOUR_LICENSE_KEY",
+          // Specify the path to the GeoLite2 database file
+          databasePath: "path/to/GeoLite2-City.mmdb",
+        });
+
+        const cityCoordinates = await Promise.all(
+          cityData.map(async ({ city, ip, time }) => {
+            const location = await geocoder.lookupByName(city);
+            if (location) {
+              const formattedTime = time.split(" ")[0];
+              return [
+                city,
+                location.location.latitude,
+                location.location.longitude,
+                ip,
+                formattedTime,
+              ];
+            }
+            return null;
+          })
+        );
+
+        const filteredCoordinates = cityCoordinates.filter(
+          (coordinate) => coordinate !== null
+        );
+
+        setMapData({
+          city: filteredCoordinates.map((coordinate) => coordinate[0]),
+          lat: filteredCoordinates.map((coordinate) => coordinate[1]),
+          lon: filteredCoordinates.map((coordinate) => coordinate[2]),
+          customData: filteredCoordinates.map((coordinate) => [
+            coordinate[3],
+            coordinate[4],
+          ]),
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const toggleMap = () => {
+    setShowMap(!showMap);
+  };
+
+  const renderMap = () => {
+    if (!mapData) {
+      return <div>Loading...</div>;
+    }
+
+    const data = [
+      {
+        type: "scattermapbox",
+        lat: mapData.lat,
+        lon: mapData.lon,
+        mode: "markers",
+        marker: { color: "forestgreen", size: 10 },
+        text: mapData.city,
+        hoverinfo: "text+lon+lat",
+        hovertemplate:
+          "<b>%{text}</b><br>IP: %{customdata[0]}<br>Time: %{customdata[1]}<extra></extra>",
+        customdata: mapData.customData,
+      },
+    ];
+
+    const layout = {
+      mapbox: { style: "open-street-map", center: { lat: 0, lon: 0 }, zoom: 1 },
+      margin: { r: 0, t: 0, l: 0, b: 0 },
+    };
+
+    return <Plot data={data} layout={layout} />;
+  };
+
   return (
     <div id="portfolio" className="text-center">
       <div className="container">
@@ -14,7 +110,18 @@ export const Gallery = (props) => {
         </div>
         <div className="row">
           <div className="portfolio-items">
-            {props.data
+            <div className="col-sm-6 col-md-4 col-lg-4">
+              <div onClick={toggleMap}>
+                <h3>Interactive Map</h3>
+                <p>Click to view the interactive map.</p>
+              </div>
+              {showMap && (
+                <div className="map-overlay" onClick={toggleMap}>
+                  <div className="map-container">{renderMap()}</div>
+                </div>
+              )}
+            </div>
+            {/* {props.data
               ? props.data.map((d, i) => (
                   <div
                     key={`${d.title}-${i}`}
@@ -27,7 +134,7 @@ export const Gallery = (props) => {
                     />
                   </div>
                 ))
-              : "Loading..."}
+              : "Loading..."} */}
           </div>
         </div>
       </div>
